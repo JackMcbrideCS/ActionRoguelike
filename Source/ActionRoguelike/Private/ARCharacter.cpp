01c371206d5233df881d324694f2903b0391ee12
JackMcbrideCS <jackmcbridecs@gmail.com>
2024-10-28 20:06:45 +0000

    - Lecture 3.1
    - Added Debug Arrows for movement
    - Changed Camera to be decoupled from character rotation

--- Source/ActionRoguelike/Private/ARCharacter.cpp
+++ Source/ActionRoguelike/Private/ARCharacter.cpp
@@ -3,22 +3,28 @@
 
 #include "ARCharacter.h"
 
-#include "Camera/CameraComponent.h"
-#include "GameFramework/SpringArmComponent.h"
+#include "DrawDebugHelpers.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AARCharacter::AARCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("Spring Arm Component");
+	SpringArmComponent->bUsePawnControlRotation = true;
 	SpringArmComponent->SetupAttachment(RootComponent);
 	
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera Component");
 	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
@@ -28,7 +34,7 @@ void AARCharacter::BeginPlay()
 
 	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
 	{
-		ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+		const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer))
 		{
 			Subsystem->AddMappingContext(DefaultMappingContext, 0);
@@ -38,21 +44,27 @@ void AARCharacter::BeginPlay()
 
 void AARCharacter::Move(const FInputActionValue& Value)
 {
-	FVector2D InputValue = Value.Get<FVector2D>();
-	AddMovementInput(GetActorForwardVector(), InputValue.Y);
+	const FVector2D InputValue = Value.Get<FVector2D>();
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	const FVector ForwardVector = FRotationMatrix(ControlRotation).GetUnitAxis(EAxis::X);
+	const FVector RightVector = FRotationMatrix(ControlRotation).GetUnitAxis(EAxis::Y);
+	
+	AddMovementInput(ForwardVector, InputValue.Y);
+	AddMovementInput(RightVector, InputValue.X);
 }
 
 void AARCharacter::Look(const FInputActionValue& Value)
 {
-	FVector2D InputValue = Value.Get<FVector2D>();
+	const FVector2D InputValue = Value.Get<FVector2D>();
 	AddControllerYawInput(InputValue.X);
+	AddControllerPitchInput(InputValue.Y);
 }
 
 // Called every frame
 void AARCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	DrawDebugRotationArrows();
 }
 
 // Called to bind functionality to input
@@ -65,5 +77,24 @@ void AARCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AARCharacter::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AARCharacter::Look);
 	}
+}
+
+void AARCharacter::DrawDebugRotationArrows() const
+{
+	constexpr float DrawScale = 100.0f;
+	constexpr float Thickness = 5.0f;
+	constexpr float Length = 100.0f;
+	constexpr float OffsetDistance = 100.0f;
+
+	const FVector LineOffset = GetActorRightVector() * OffsetDistance;
+	const FVector LineStart = GetActorLocation() + LineOffset;
+	
+	const FVector PlayerLine = GetActorForwardVector() * Length;
+	const FVector PlayerLineEnd = LineStart + PlayerLine;
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, PlayerLineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+	
+	const FVector ControllerLine = GetControlRotation().Vector() * Length;
+	const FVector ControllerLineEnd = LineStart + ControllerLine;
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerLineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
 }
 

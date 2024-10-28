be365b9c1dab8105cc8048c8eb07393cc4a29ccf
JackMcbrideCS <jackmcbridecs@gmail.com>
2024-10-28 22:19:41 +0000

    - Lecture 4.1
    - Create Gameplay Interface and Item Chest

---
+++ Source/ActionRoguelike/Private/ARItemChest.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARItemChest.h"
+
+
+void AARItemChest::Interact_Implementation(APawn* InstigatorPawn)
+{
+	LidMesh->SetRelativeRotation(FRotator(TargetPitch, 0, 0));
+}
+
+// Sets default values
+AARItemChest::AARItemChest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("Base Mesh");
+	RootComponent = BaseMesh;
+
+	LidMesh = CreateDefaultSubobject<UStaticMeshComponent>("Lid Mesh");
+	LidMesh->SetupAttachment(BaseMesh);
+
+	TargetPitch = 110.0f;
+}
+
+// Called when the game starts or when spawned
+void AARItemChest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AARItemChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ActionRoguelike/Public/ARItemChest.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ARGameplayInterface.h"
+#include "GameFramework/Actor.h"
+#include "ARItemChest.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API AARItemChest : public AActor, public IARGameplayInterface
+{
+	GENERATED_BODY()
+
+	void Interact_Implementation(APawn* InstigatorPawn);
+
+public:
+	UPROPERTY(EditAnywhere)
+	float TargetPitch;
+	
+	// Sets default values for this actor's properties
+	AARItemChest();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* LidMesh;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
